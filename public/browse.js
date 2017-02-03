46def249cdccbcfce1d9a714fbfec3cd0f9d8251
vita newstetter <vitanewstetter@ucla.edu>
2017-02-02 20:59:19 -0800

    made ejs files, kept html files though. added server, added facebook authenticator

---
+++ public/browse.js
@@ -0,0 +1,82 @@
+
+//variables to pass along
+var selections= [];
+
+var keywords = document.getElementById("browse-keywords");
+var currentlySelected = document.getElementById("currently-selected");
+var closeSearch = document.getElementById("close-search");
+var selector = document.querySelectorAll(".browse-selector");
+var selectedX;
+var onX = false;
+var more = document.querySelectorAll(".more");
+var final = document.querySelectorAll(".final");
+
+
+//show search bar on click
+currentlySelected.onclick = function(){
+    if(onX == false){keywords.style.display = "block";}};
+
+//hide search bar on X
+closeSearch.onclick = function(){ keywords.style.display = "none";};
+
+//mouseover show dropdown menu
+// the "-2" excludes the checkboxes all the way on the right!!!
+for (var j = 0; j < selector.length-2; j++) {
+    selector[j].onmouseover = function () {
+        var dropdown = this.querySelectorAll(".browse-dropdown");
+        dropdown[0].style.display = "block";
+    };
+    selector[j].onmouseout = function () {
+        var dropdown = this.querySelectorAll(".browse-dropdown");
+        dropdown[0].style.display = "none";
+        var dropdown2 = this.querySelectorAll(".browse-dropdown2");
+        dropdown2[0].style.display = "none";
+    };
+}
+//show dropdown menu layer two
+var dropdown2 = function(){
+    for (var m = 0; m < more.length; m++) {
+        more[m].onmouseover = function () {
+            var dropdown = this.querySelectorAll(".browse-dropdown2");
+            dropdown[0].style.display = "block";
+        };
+        more[m].onmouseout = function () {
+            console.log("hi");
+            var dropdown = this.querySelectorAll(".browse-dropdown2");
+                dropdown[0].style.display = "none";
+        };
+    }
+};
+dropdown2();
+
+//remove selected terms by clicking x
+var removeSelection = function(){
+    selectedX = document.querySelectorAll(".selected-x");
+    for (var k = 0; k < selectedX.length; k++){
+        selectedX[k].onclick = function(){
+            onX = true;
+            var child = this.parentNode;
+            var parent = child.parentNode;
+            parent.removeChild(child);
+        };
+        //make sure search bar doesn't show
+        currentlySelected.onmousemove = function(){
+            onX = false;
+        };
+    }
+};
+removeSelection();
+//add query when you click on dropdown menu
+for (var l = 0; l < final.length; l++){
+    final[l].onclick = function(){
+        var name = this.innerHTML;
+        var html = name + '<img class = "x-out selected-x" src="images/icons/x.svg">';
+        var newSelection = document.createElement('div');
+        newSelection.className = "browse-selection";
+        newSelection.innerHTML = html;
+        currentlySelected.appendChild(newSelection);
+        removeSelection();
+        selectedX = document.querySelectorAll(".selected-x");
+    };
+}
+
--- server.js
+++ server.js
@@ -1,3 +1,108 @@
-/**
- * Created by vita on 1/31/17.
- */
+var express = require("express"),
+    // fs = require("fs"),
+    // path = require("path"),
+    passport = require("passport"),
+    facebook = require("passport-facebook").Strategy;
+    // google = require("passport-google-oauth").Strategy;
+    //MongoClient = require("mongodb").MongoClient,
+    //cors = require("cors"),
+    //shortid = require('shortid');
+
+passport.use(new facebook({
+        clientID: '239772336432810',
+        clientSecret: 'b9c2fac8c81daeec2356ba92d5f7b88a',
+        callbackURL: 'http://localhost:3000/login/facebook/return'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        // In this example, the user's Facebook profile is supplied as the user
+        // record.  In a production-quality application, the Facebook profile should
+        // be associated with a user record in the application's database, which
+        // allows for account linking and authentication with other identity
+        // providers.
+        return cb(null, profile);
+    }));
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+var app = express();
+
+app.set('view engine', 'ejs');
+//says directory where to find static files
+app.use(express.static('public'));
+
+//middleware
+    app.use(require('morgan')('combined'));
+    app.use(require('cookie-parser')());
+    app.use(require('body-parser').urlencoded({ extended: true }));
+    app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", function(req, res){
+    //UNCOMMENT BELOW TO CLEAR DB
+    //db.collection('cards').remove({});
+    console.log(req.method);
+    console.log("index");
+    console.log(req.user);
+        res.render('index', { user: req.user });
+});
+//load browse page
+app.get("/browse", function(req, res){
+    console.log(req.method);
+    console.log("browse");
+    res.render('browse'), { user: req.user };
+});
+
+
+//go to facebook login
+app.get('/logout', function(req, res){
+    console.log(req);
+    req.logout();
+    res.redirect('/');
+});
+
+app.get('/login',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+        console.log(res);
+    });
+
+app.get('/login/facebook',
+    passport.authenticate('facebook'));
+
+app.get('/login/facebook/return',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+app.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+        res.render('profile', { user: req.user });
+    });
+
+app.listen(3000, function(req, res){
+    console.log("We are listening on port 3000!");
+
+});
